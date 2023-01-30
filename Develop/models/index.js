b4d8119aef488c74e01ca1dd83454e426412b283
Sofia92 <sofiasosaperez@outlook.com>
2023-01-29 20:42:00 -0600

    models index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,3 +1,5 @@
+///??? check this 
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,12 +7,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE', ///??? check this 
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: '',
+});
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  foreignKey: '',
+});
+
 
 module.exports = {
   Product,
