1b2432b046ed90be3f82ceee9320c51e2468cf2a
Sofia92 <sofiasosaperez@outlook.com>
2023-01-31 16:58:59 -0600

    finished product-routes.js

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,9 +5,9 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
+  // be sure to include its associated Products
       include: [{ model: Product }],
     });
     res.status(200).json(categoryData);
@@ -16,14 +16,14 @@ router.get('/', async (req, res) => {
   }
 });
 
+// find one category by its `id` value
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   //findByPk means that it fetches single database record
   try {
     const categoryData = await Category.findByPk(req.params.id, {
+    // be sure to include its associated Products
       include: [{ model: Product }],
-    })
+    });
     if (!categoryData) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
@@ -35,8 +35,8 @@ router.get('/:id', async (req, res) => {
 });
 
 
+// create a new category
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
