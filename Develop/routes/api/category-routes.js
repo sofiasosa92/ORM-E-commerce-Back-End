4c5b5e192e69aa720d472a40ed34df98bb21aefe
Sofia92 <sofiasosaperez@outlook.com>
2023-01-31 16:28:22 -0600

    category-routes done

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
+// The `/api/categories` endpoint
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
@@ -10,24 +10,20 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
     });
-
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // be sure to include its associated Products
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-
   // be sure to include its associated Products
   //findByPk means that it fetches single database record
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
-    });
-
+    })
     if (!categoryData) {
       res.status(404).json({ message: 'No category with this id!' });
       return;
@@ -38,14 +34,11 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+
 router.post('/', async (req, res) => {
   // create a new category
-  //?????????????? check/ask about this one
   try {
-    const categoryData = await Category.create({
-      id: req.body.id,
-      category_name: req.body.category_name,
-    });
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -73,7 +66,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const categoryData = await User.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       },
